b9e060cc79a7c244d1a3d9f656f770da154f269a
Rainer Volz <dev@textmulch.de>
2017-04-27 18:21:59 +0200

    Add handling of multiple front matter pages.
    * The attribute "fm-files" can contain an array of filenames that will be inserted as EPUB front matter pages
    * If the attribute is not set the package looks for a single "front-matter.html" file (current behavior)

--- lib/asciidoctor-epub3/packager.rb
+++ lib/asciidoctor-epub3/packager.rb
@@ -190,21 +190,45 @@ body > svg {
     nil
   end
 
-  def add_images_from_front_matter
-    (::File.read 'front-matter.html').scan ImgSrcScanRx do
-      resources do
-        file $1
+
+  def get_frontmatter_files doc
+    if doc.attr? 'fm-files'
+      fms = doc.attributes['fm-files']
+      if fms.is_a? Array
+        fms
+      else
+        warn %(Front matter attribute 'fm-files' must contain an Array! Ignoring ...)
+        []
       end
-    end if ::File.file? 'front-matter.html'
+    elsif File.exist? 'front-matter.html'
+      ['front-matter.html']
+    else
+      []
+    end
+  end
+
+  def add_images_from_front_matter doc
+    get_frontmatter_files(doc).each do |fmf|
+      (::File.read fmf).scan ImgSrcScanRx do
+        resources do
+          file $1
+        end
+      end if ::File.file? fmf
+    end
     nil
   end
 
   def add_front_matter_page doc, spine_builder
-    if ::File.file? 'front-matter.html'
-      front_matter_content = ::File.read 'front-matter.html'
-      spine_builder.file 'front-matter.xhtml' => (postprocess_xhtml front_matter_content, @format)
-      unless (spine_builder.property? 'svg') || SvgImgSniffRx !~ front_matter_content
-        spine_builder.add_property 'svg'
+    get_frontmatter_files(doc).each do |fmf|
+      if ::File.exist? fmf
+        fn = File.basename(fmf)
+        front_matter_content = ::File.read fmf
+        spine_builder.file fn => (postprocess_xhtml front_matter_content, @format)
+        unless (spine_builder.property? 'svg') || SvgImgSniffRx !~ front_matter_content
+          spine_builder.add_property 'svg'
+        end
+      else
+        warn %(Looking for front matter: file #{fmf} is configured but doesn't exist!)
       end
     end
     nil
@@ -270,7 +294,7 @@ body > svg {
     workdir = (doc.attr 'docdir').nil_or_empty? ? '.' : workdir
     resources workdir: workdir do
       extend GepubResourceBuilderMixin
-      builder.add_images_from_front_matter
+      builder.add_images_from_front_matter doc
       builder.add_nav_doc doc, self, spine, format
       builder.add_ncx_doc doc, self, spine
       ordered do
